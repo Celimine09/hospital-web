7fec5860f3cf3aa607ecdfded1e17d9738142209
Madara <elio.interesante@gmail.com>
2024-02-27 02:30:51 +0700

    adjust home.css for more responsive.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,20 +1,29 @@
-import '../app/styles/home.css';
+import "../app/styles/home.css";
 
 const Homepage = () => {
-  
   return (
-    <><nav className="navbar">
-      <div className="navbar-links">
-        <a href="#">จัดการข้อมูลคนไข้</a>
-        <a href="#">จัดการข้อมูลบุคลากร</a>
-        <a href="#">ดูห้องว่าง</a>
-      </div>
-    </nav><img className="img1" src="https://static.hd.co.th/system/hospitals/image2s/000/000/129/original/patrangsit-hospital-02.jpg" /><div className="box-container">
+    <div style={{
+      height: "100vh",
+      backgroundColor: "red"
+    }}>
+      <nav className="navbar">
+        <div className="navbar-links">
+          <a href="#">จัดการข้อมูลคนไข้</a>
+          <a href="#">จัดการข้อมูลบุคลากร</a>
+          <a href="#">ดูห้องว่าง</a>
+        </div>
+      </nav>
+      <img
+        className="img1"
+        src="https://static.hd.co.th/system/hospitals/image2s/000/000/129/original/patrangsit-hospital-02.jpg"
+      />
+      <div className="box-container">
         <div className="box1"></div>
         <div className="box2"></div>
         <div className="box3"></div>
         <div className="box4"></div>
-      </div></>
+      </div>
+    </div>
   );
 };
 
