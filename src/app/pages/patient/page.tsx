ef003c8d366098da314dfa4e7ef2f76c414e7c1c
bell <ppasikapong@gmail.com>
2024-03-10 06:20:58 +0700

    staff page

--- src/app/pages/patient/page.tsx
+++ src/app/pages/patient/page.tsx
@@ -3,51 +3,50 @@ import MySearchBar from "@/app/components/searchbar/MySearchBar"
 import ServerSidePersistence from "@/app/components/editable_grid/patient_grid"
 import { useEffect, useState } from "react"
 
-interface IHero
-{
+interface IHero {
     heroName: string
     attackType: string
 }
 
 const page = () => {
-  // let n : number = 10
-  const [n, setN] = useState<number>(0)
-  const dummyHeros : IHero[] = [{heroName : "A", attackType: "A"}]
+    // let n : number = 10
+    const [n, setN] = useState<number>(0)
+    const dummyHeros: IHero[] = [{ heroName: "A", attackType: "A" }]
 
 
-  const onButtonClicked = () => {
-      console.log("clicked.")
-      // n += 1
-      setN(n + 1)
-      // getX()
-  }
+    const onButtonClicked = () => {
+        console.log("clicked.")
+        // n += 1
+        setN(n + 1)
+        // getX()
+    }
 
-  useEffect(() => {
-  }, [])
+    useEffect(() => {
+    }, [])
 
 
 
-  return <div>
-      {/* <TikTok isTick={true} size={64}/>
+    return <div>
+        {/* <TikTok isTick={true} size={64}/>
       <TikTok isTick={false}/> */}
-      
-      {/* <FullFeaturedCrudGrid /> */}
-      {/* <BasicColumnsGrid /> */}
-      <div style={{
-          width: "80vw",
-          height: "80vh",
-          // display: "flex",
-          // justifyContent: "center",
-          // alignItems: "center",
-          // backgroundColor: "red",
-          marginLeft: "auto",
-          marginRight: "auto",
-          //marginTop: "auto"
 
-      }}>
-          <ServerSidePersistence />
-      </div>
-  </div>
+        {/* <FullFeaturedCrudGrid /> */}
+        {/* <BasicColumnsGrid /> */}
+        <div style={{
+            width: "80vw",
+            height: "80vh",
+            // display: "flex",
+            // justifyContent: "center",
+            // alignItems: "center",
+            // backgroundColor: "red",
+            marginLeft: "auto",
+            marginRight: "auto",
+            //marginTop: "auto"
+
+        }}>
+            <ServerSidePersistence />
+        </div>
+    </div>
 }
 
 export default page
