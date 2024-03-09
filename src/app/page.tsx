4a4d0a330d3e1d30b303242a706fb1ca2aac2da8
Madara <elio.interesante@gmail.com>
2024-03-09 17:15:57 +0700

    before push to tonam

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,28 @@ export default function Home() {
     //   mainmain
     // </main>
     <>
-    <h1>
-      hey
-    </h1>
+      <div style={{
+        height: "100vh",
+        backgroundColor: "red"
+      }}>
+        <nav className="navbar">
+          <div className="navbar-links">
+            <a href="#">จัดการข้อมูลคนไข้</a>
+            <a href="#">จัดการข้อมูลบุคลากร</a>
+            <a href="#">ดูห้องว่าง</a>
+          </div>
+        </nav>
+        <img
+          className="img1"
+          src="https://static.hd.co.th/system/hospitals/image2s/000/000/129/original/patrangsit-hospital-02.jpg"
+        />
+        <div className="box-container">
+          <div className="box1"></div>
+          <div className="box2"></div>
+          <div className="box3"></div>
+          <div className="box4"></div>
+        </div>
+      </div>
     </>
   );
 }
