6e818233a0b96de4c49da805812ebac1810f66a3
bell <ppasikapong@gmail.com>
2024-03-10 05:08:11 +0700

    staff page

---
+++ src/app/pages/staff/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MySearchBar from '@/app/components/searchbar/MySearchBar'
+
+const page = () => {
+    return (
+        <div>
+            <MySearchBar />
+        </div>
+    )
+}
+
+export default page
