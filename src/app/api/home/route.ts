1b1dbe9c286c25701abcf784f6959e435f41a39e
tonnamnam <snowyfoxproduction@gmail.com>
2024-03-11 20:31:55 +0700

    nothing

--- src/app/api/home/route.ts
+++ src/app/api/home/route.ts
@@ -4,16 +4,16 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     try {
-        const [results1, fields1] = await connection.query('select count(*) from Room r');
-        console.log(results1);
+        const [results1, fields1] = await connection.query<RowDataPacket[]>('select count(*) from Room r');
+        console.log(results1[0]);
         console.log(fields1);
     } catch (err) {
         console.log(err);
     }
 
     try {
-        const [results2, fields2] = await connection.query('select count(*) from Staff s');
-        console.log(results2);
+        const [results2, fields2] = await connection.query<RowDataPacket[]>('select count(*) from Staff s');
+        console.log(results2[0]);
         console.log(fields2);
     } catch (err) {
         console.log(err);
