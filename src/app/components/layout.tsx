39b819362533287426b5294fccfc0515d511ab21
Madara <elio.interesante@gmail.com>
2024-03-15 22:51:45 +0700

    added dark theme button

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -1,16 +1,34 @@
 'use client'
 import React, { PropsWithChildren, useEffect, useState } from "react";
 import Navbar from "./navbar";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { PaletteMode } from "@mui/material";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ColorModeContext } from "../contexts/theme.context";
 
-const darkTheme = createTheme({
-    palette: {
-        mode: 'dark',
-    },
-});
 
 const Layout = ({ children }: PropsWithChildren) => {
+    const [mode, setMode] = React.useState<PaletteMode>('light');
+    const colorMode = React.useMemo(
+        () => ({
+            toggleColorMode: () => {
+                setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+            },
+        }),
+        [],
+    );
+
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode,
+                },
+            }),
+        [mode],
+    );
+
+
     const [loggingIn, setLoggingIn] = useState(false);
 
     useEffect(() => {
@@ -22,11 +40,14 @@ const Layout = ({ children }: PropsWithChildren) => {
 
     return (
         <>
-            <ThemeProvider theme={darkTheme}>
-                <CssBaseline />
-                {loggingIn && <Navbar />}
-                {children}
-            </ThemeProvider>
+            <ColorModeContext.Provider value={colorMode}>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    {loggingIn && <Navbar />}
+                    {children}
+                </ThemeProvider>
+
+            </ColorModeContext.Provider>
         </>
     );
 };
